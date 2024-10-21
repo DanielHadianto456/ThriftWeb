08f45f4f08060915441d0cab451f8b849bed9d8e
Daniel Hadianto <daniel88hadianto@gmail.com>
2024-10-21 14:28:07 +0700

    Feat: Added CRUD for Pakaian

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\kategoriController;
+use App\Http\Controllers\pakaianController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -18,6 +19,14 @@ Route::middleware('auth:user_model')->group(function(){
         Route::patch('admin/kategori/edit/{id}', 'editCategory');
         Route::delete('admin/kategori/delete/{id}', 'deleteCategory');
 
+    });
+
+    Route::controller(pakaianController::class)->group(function(){
+        Route::get('admin/pakaian', 'getAll');
+        Route::post('admin/pakaian/add', 'addPakaian');
+        Route::patch('admin/pakaian/edit/{id}', 'editPakaian');
+        Route::delete('admin/pakaian/delete/{id}', 'deletePakaian');
+        Route::post('admin/pakaian/add-stock/{id}', 'addStock'); // Add this line
     });
     
 });
