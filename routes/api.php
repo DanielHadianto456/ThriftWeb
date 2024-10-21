b7b1d5c5b0b21aefc25b3fde22afc4ccbdbe4644
Daniel Hadianto <daniel88hadianto@gmail.com>
2024-10-21 13:27:55 +0700

    Feat: Added CRUD for Category

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\kategoriController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -10,6 +11,14 @@ Route::get('/user', function (Request $request) {
 
 Route::middleware('auth:user_model')->group(function(){
 
+    Route::controller(kategoriController::class)->group(function(){
+
+        Route::get('admin/kategori', 'getAll');
+        Route::post('admin/kategori/add', 'addCategory');
+        Route::patch('admin/kategori/edit/{id}', 'editCategory');
+        Route::delete('admin/kategori/delete/{id}', 'deleteCategory');
+
+    });
     
 });
 Route::controller( AuthController::class)->group(function(){
